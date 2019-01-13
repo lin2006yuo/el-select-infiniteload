bcaf53dd93bbbbbeeeb79f4fd9280c25ab226948
lin2006yuo <547677504@qq.com>
2019-01-13 09:19:43 +0800

    input number

--- src/views/component.vue
+++ src/views/component.vue
@@ -5,12 +5,12 @@
           <split-pane :value.sync="offset"></split-pane>
       </div>
       <p>ElementUI</p>
-      <el-input v-range.number="{max: 8, min: 0}" ref="input1" :controls="false" :value="num1" @keyup.native="handleInput"></el-input>
-      <el-input-number :controls="false" :value="num2" @keyup.native="handleInput"></el-input-number>
+      <el-input v-range.number="{max: 8, min: 0}" @keyup.native="handleInput(2, $event)" :value="num1"></el-input>
+      <el-input v-range.number="{max: 8, min: 0}" @keyup.native="handleInput2(2, $event)" :value="num2"></el-input>
       <!-- <p>原生</p>
       <input ref="input1" type="text" :value="c_num1" @keyup="handleKeyup" @input="hanleRawInput">
       <input ref="input2" type="text" :value="num2" @keyup="handleKeyup"> -->
-      num1: {{num1}}
+      num1: {{num1}}<br>
       num2: {{num2}}
   </div>
 </template>
@@ -22,19 +22,15 @@ export default {
     directives: {
         range: {
             bind(el, binding) {
-                console.log(binding.modifiers)
                 const {max, min} = binding.value
-                const INPUT_DOM = el.querySelector('input')
+                const INPUT_DOM = el.querySelector('input');
                 const handleInput = (e) => {
                     if(binding.modifiers.number) {
-                        INPUT_DOM.value = INPUT_DOM.value.replace(/[^1-9]/g, "")
+                        INPUT_DOM.value = INPUT_DOM.value.replace(/[^0-9]/g, "")
                     }
                     if(INPUT_DOM.value > max) {
-                        console.log('大于')
-                        INPUT_DOM.value = 8
-                        console.log(INPUT_DOM.value)
+                        INPUT_DOM.value = 8;
                     } else if(INPUT_DOM.value < min) {
-                        console.log('小于')
                         INPUT_DOM.value = 0
                     }
                 }
@@ -47,8 +43,9 @@ export default {
   },
   data() {
     return {
+        max: 8,
         offset: 0.8,
-        num1: 1,
+        num1: 0,
         num2: 2,
         sum: 8
     };
@@ -59,10 +56,17 @@ export default {
       }
   },
   methods: {
-      handleInput(e) {
+      handleInput(max,e) {
          this.$nextTick(() => {
-            this.num1 = e.target.value
+            this.num1 = e.target.value;
+            this.num2 = max - e.target.value;
          })
+      },
+      handleInput2(max,e) {
+          this.$nextTick(() => {
+              this.num2 = e.target.value;
+              this.num1 = max - e.target.value;
+          })
       }
   }
 };
